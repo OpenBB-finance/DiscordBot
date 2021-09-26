2af2acb01c82a3fd0bb037bf6e900a9c2e54605d
didier <dro.lopes@campus.fct.unl.pt>
2021-09-27 00:13:19 +0100

    Improve shorted and hsi commands from dps

--- stocks/dark_pool_shorts/menu.py
+++ stocks/dark_pool_shorts/menu.py
@@ -1,21 +1,7 @@
-from discord.ext import commands
 import discord
-from helpers import pagination
 from discordbot import gst_bot
 import config_discordbot as cfg
-import helpers
-import os
 import asyncio
-from datetime import datetime, timedelta
-from matplotlib import pyplot as plt
-import matplotlib.dates as mdates
-import pyimgur
-
-im = pyimgur.Imgur(cfg.IMGUR_CLIENT_ID)
-
-from gamestonk_terminal.stocks.dark_pool_shorts import stockgrid_model
-
-from gamestonk_terminal.config_plot import PLOT_DPI
 
 from stocks.dark_pool_shorts.shorted import shorted_command
 from stocks.dark_pool_shorts.ftd import ftd_command
@@ -27,21 +13,32 @@ from stocks.dark_pool_shorts.pos import pos_command
 from stocks.dark_pool_shorts.sidtc import sidtc_command
 
 
-class DarkPoolShortsCommands(commands.Cog):
+class DarkPoolShortsCommands(discord.ext.commands.Cog):
     """Dark Pool Shorts menu."""
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
 
-    @commands.command(name="stocks.dps.shorted")
-    async def shorted(self, ctx: commands.Context, arg=""):
-        if arg:
-            await shorted_command(ctx, arg)
-        else:
-            await shorted_command(ctx)
+    @discord.ext.commands.command(name="stocks.dps.shorted")
+    async def shorted(self, ctx: discord.ext.commands.Context, arg=""):
+        await shorted_command(ctx, arg)
 
-    @commands.command(name="stocks.dps.ftd")
-    async def ftd(self, ctx: commands.Context, arg, arg2="", arg3=""):
+    @discord.ext.commands.command(name="stocks.dps.hsi")
+    async def hsi(self, ctx: discord.ext.commands.Context, arg=""):
+        await hsi_command(ctx, arg)
+
+    @discord.ext.commands.command(name="stocks.dps.pos")
+    async def pos(self, ctx: discord.ext.commands.Context, arg="10", arg2="dpp_dollar"):
+        arg = int(arg)
+        await pos_command(ctx, arg, arg2)
+
+    @discord.ext.commands.command(name="stocks.dps.sidtc")
+    async def sidtc(self, ctx: discord.ext.commands.Context, arg="10", arg2="float"):
+        arg = int(arg)
+        await sidtc_command(ctx, arg, arg2)
+
+    @discord.ext.commands.command(name="stocks.dps.ftd")
+    async def ftd(self, ctx: discord.ext.commands.Context, arg, arg2="", arg3=""):
         if arg2:
             if arg3:
                 embed = ftd_command(arg, arg2, arg3)
@@ -54,8 +51,8 @@ class DarkPoolShortsCommands(commands.Cog):
                 embed = ftd_command(arg)
         await ctx.send(embed=embed)
 
-    @commands.command(name="stocks.dps.dpotc")
-    async def dpotc(self, ctx: commands.Context, arg, arg2="", arg3=""):
+    @discord.ext.commands.command(name="stocks.dps.dpotc")
+    async def dpotc(self, ctx: discord.ext.commands.Context, arg, arg2="", arg3=""):
         if arg2:
             if arg3:
                 embed = dpotc_command(arg, arg2, arg3)
@@ -68,42 +65,39 @@ class DarkPoolShortsCommands(commands.Cog):
                 embed = dpotc_command(arg)
         await ctx.send(embed=embed)
 
-    @commands.command(name="stocks.dps.spos")
-    async def spos(self, ctx: commands.Context, arg):
+    @discord.ext.commands.command(name="stocks.dps.spos")
+    async def spos(self, ctx: discord.ext.commands.Context, arg):
         embed = spos_command(arg)
         await ctx.send(embed=embed)
 
-    @commands.command(name="stocks.dps.psi")
-    async def psi(self, ctx: commands.Context, arg):
+    @discord.ext.commands.command(name="stocks.dps.psi")
+    async def psi(self, ctx: discord.ext.commands.Context, arg):
         embed = psi_command(arg)
         await ctx.send(embed=embed)
 
-    @commands.command(name="stocks.dps.hsi")
-    async def hsi(self, ctx: commands.Context, arg="10"):
-        arg = int(arg)
-        await hsi_command(ctx, arg)
+    # !stocks.dps.pos <NUM> (default: 10) SORT (default: dpp_dollar; options: sv,sv_pct,nsv,nsv_dollar,dpp,dpp_dollar
+    # !stocks.dps.sidtc NUM (default: 10) SORT (default: float; options: float,dtc,si)
 
-    @commands.command(name="stocks.dps.pos")
-    async def pos(self, ctx: commands.Context, arg="10", arg2="dpp_dollar"):
-        arg = int(arg)
-        await pos_command(ctx, arg, arg2)
-
-    @commands.command(name="stocks.dps.sidtc")
-    async def sidtc(self, ctx: commands.Context, arg="10", arg2="float"):
-        arg = int(arg)
-        await sidtc_command(ctx, arg, arg2)
-
-    @commands.command(name="stocks.dps")
-    async def dark_pool_shorts_menu(self, ctx: commands.Context):
+    @discord.ext.commands.command(name="stocks.dps")
+    async def dark_pool_shorts_menu(self, ctx: discord.ext.commands.Context, arg=""):
         text = (
-            "0️⃣ !stocks.dps.shorted NUM (default: 5)\n\n1️⃣ !stocks.dps.hsi NUM (default: 10)\n\n2️⃣ !stocks.dps.pos NUM "
-            "(default: 10) SORT (default: dpp_dollar; options: sv,sv_pct,nsv,nsv_dollar,dpp,dpp_dollar)\n\n3️⃣ "
-            "!stocks.dps.sidtc NUM (default: 10) SORT (default: float; options: float,dtc,si)\n\nCommands below "
-            "require the input TICKER and must be entered directly:\n(DATE format: "
-            ")\n\n4️⃣"
-            "!stocks.dps.ftd TICKER DATE_START DATE_END\n\n5️⃣ !stocks.dps.dpotc TICKER DATE_START DATE_END\n\n6️⃣ "
-            "!stocks.dps.spos TICKER\n\n7️⃣ !stocks.dps.psi TICKER"
+            "0️⃣ !stocks.dps.shorted <NUM>\n"
+            "1️⃣ !stocks.dps.hsi <NUM>\n"
+            "2️⃣ !stocks.dps.pos <NUM> <SORT>\n"
+            "3️⃣ !stocks.dps.sidtc <NUM> <SORT>\n"
         )
+        if arg:
+            text += (
+                "4️⃣ !stocks.dps.ftd <TICKER> <DATE_START> <DATE_END>\n"
+                "5️⃣ !stocks.dps.dpotc <TICKER> <DATE_START> <DATE_END>\n"
+                "6️⃣ !stocks.dps.spos <TICKER>\n"
+                "7️⃣ !stocks.dps.psi <TICKER>\n"
+            )
+        else:
+            text += (
+                "\nMore commands available when providing a ticker with:"
+                "\n!stocks.dps <TICKER>"
+            )
 
         title = "Dark Pool Shorts (DPS) Menu"
         embed = discord.Embed(title=title, description=text, colour=cfg.COLOR)
@@ -115,10 +109,11 @@ class DarkPoolShortsCommands(commands.Cog):
 
         emoji_list = ["0️⃣", "1️⃣", "2️⃣", "3️⃣"]
 
-        await msg.add_reaction("0️⃣")
-        await msg.add_reaction("1️⃣")
-        await msg.add_reaction("2️⃣")
-        await msg.add_reaction("3️⃣")
+        if arg:
+            emoji_list += ["4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣"]
+
+        for emoji in emoji_list:
+            await msg.add_reaction(emoji)
 
         def check(reaction, user):
             return user == ctx.message.author and str(reaction.emoji) in emoji_list
@@ -142,5 +137,5 @@ class DarkPoolShortsCommands(commands.Cog):
             await msg.edit(embed=embed)
 
 
-def setup(bot: commands.Bot):
+def setup(bot: discord.ext.commands.Bot):
     gst_bot.add_cog(DarkPoolShortsCommands(bot))
--- stocks/dark_pool_shorts/shorted.py
+++ stocks/dark_pool_shorts/shorted.py
@@ -5,43 +5,65 @@ from helpers import pagination
 from gamestonk_terminal.stocks.dark_pool_shorts import yahoofinance_model
 
 
-async def shorted_command(ctx, arg="5"):
-    df = yahoofinance_model.get_most_shorted().head(int(arg))
+async def shorted_command(ctx, arg=""):
+    # Select default
+    if not arg:
+        arg = "5"
 
-    df.dropna(how="all", axis=1, inplace=True)
-    df = df.replace(float("NaN"), "")
-    future_column_name = df["Symbol"]
-    df = df.transpose()
-    df.columns = future_column_name
-    df.drop("Symbol")
-    columns = []
-
-    initial_str = "Page 0: Overview"
-    i = 1
-
-    for col_name in df.columns.values:
-        initial_str += f"\nPage {i}: {col_name}"
-        i += 1
-
-    columns.append(
-        discord.Embed(
-            title="[Yahoo Finance] Most Shorted Stocks",
-            description=initial_str,
+    # Help
+    if arg == "-h":
+        help_txt = "Display most shorted stocks screener. [Source: Yahoo Finance]\n"
+        help_txt += "\nPossible argments:\n"
+        help_txt += f"<NUM> Number of stocks to display. Default: 5"
+        embed = discord.Embed(
+            title="Stocks: [Yahoo Finance] Most Shorted HELP",
+            description=help_txt,
             colour=cfg.COLOR,
-        ).set_author(
+        )
+        embed.set_author(
             name=cfg.AUTHOR_NAME,
             icon_url=cfg.AUTHOR_ICON_URL,
         )
-    )
-    for column in df.columns.values:
+
+        await ctx.send(embed=embed)
+
+    else:
+        df = yahoofinance_model.get_most_shorted().head(int(arg))
+
+        df.dropna(how="all", axis=1, inplace=True)
+        df = df.replace(float("NaN"), "")
+        future_column_name = df["Symbol"]
+        df = df.transpose()
+        df.columns = future_column_name
+        df.drop("Symbol")
+        columns = []
+
+        initial_str = "Page 0: Overview"
+        i = 1
+
+        for col_name in df.columns.values:
+            initial_str += f"\nPage {i}: {col_name}"
+            i += 1
+
         columns.append(
             discord.Embed(
-                description="```" + df[column].fillna("").to_string() + "```",
+                title="Stocks: [Yahoo Finance] Most Shorted",
+                description=initial_str,
                 colour=cfg.COLOR,
             ).set_author(
                 name=cfg.AUTHOR_NAME,
                 icon_url=cfg.AUTHOR_ICON_URL,
             )
         )
+        for column in df.columns.values:
+            columns.append(
+                discord.Embed(
+                    description="```" + df[column].fillna("").to_string() + "```",
+                    colour=cfg.COLOR,
+                ).set_author(
+                    name=cfg.AUTHOR_NAME,
+                    icon_url=cfg.AUTHOR_ICON_URL,
+                )
+            )
 
-    await pagination(columns, ctx)
+        await pagination(columns, ctx)
