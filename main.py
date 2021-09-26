eacab057c35b515136b725f631ace1e0141accfe
didier <dro.lopes@campus.fct.unl.pt>
2021-09-26 01:11:43 +0100

    refactor importing modules

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
-import discord
-from discord import Client, Intents, Embed
-from discord.ext import commands
 import sys
-import os
-import pathlib
-from discord_components import DiscordComponents, Button, ButtonStyle
+import discord
+import discord_components
 import asyncio
 
 ##############
 ## Settings ##
 ##############
-DISCORD_BOT_TOKEN = 'string' # Insert your bots secrets token
-IMGUR_CLIENT_ID = 'string' # Enter your imgur client id
-command_prefix = '!' # Sets the prefix to the commands
-activity = discord.Game(name='Gametonk Terminal: https://github.com/GamestonkTerminal/GamestonkTerminal')
-gst_path = 'C:\\Users\\user\\GamestonkTerminal' # The path to Gamestonk Terminal
-date_input_format = '%Y-%m-%d' # Enter your prefered date input format
+DISCORD_BOT_TOKEN = "string"  # Insert your bots secrets token
+IMGUR_CLIENT_ID = "string"  # Enter your imgur client id
+command_prefix = "!"  # Sets the prefix to the commands
+activity = discord.Game(
+    name="Gametonk Terminal: https://github.com/GamestonkTerminal/GamestonkTerminal"
+)
+gst_path = "C:\\Users\\user\\GamestonkTerminal"  # The path to Gamestonk Terminal
+date_input_format = "%Y-%m-%d"  # Enter your prefered date input format
 
 ## Defining the bot ##
-bot = commands.Bot(command_prefix=command_prefix, intents=discord.Intents.all(), activity=activity)
-DiscordComponents(bot)
+bot = discord.ext.commands.Bot(
+    command_prefix=command_prefix, intents=discord.Intents.all(), activity=activity
+)
+discord_components.DiscordComponents(bot)
 
 bot_colour = discord.Color.from_rgb(0, 206, 154)
 
 sys.path.append(gst_path)
 
-async  def pagination(dataset_with_embeds, ctx):
+
+async def pagination(dataset_with_embeds, ctx):
     current = 0
-    components = [[Button(label="Prev", id="back", style=ButtonStyle.red),
-                   Button(label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current]))}/{len(dataset_with_embeds)}",
-                          id="cur", style=ButtonStyle.green, disabled=True),
-                   Button(label="Next", id="front", style=ButtonStyle.green)]]
-    mainMessage = await ctx.send(embed=dataset_with_embeds[current], components=components)
+    components = [
+        [
+            discord_components.Button(
+                label="Prev", id="back", style=discord_components.ButtonStyle.red
+            ),
+            discord_components.Button(
+                label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current]))}/{len(dataset_with_embeds)}",
+                id="cur",
+                style=discord_components.ButtonStyle.green,
+                disabled=True,
+            ),
+            discord_components.Button(
+                label="Next", id="front", style=discord_components.ButtonStyle.green
+            ),
+        ]
+    ]
+    mainMessage = await ctx.send(
+        embed=dataset_with_embeds[current], components=components
+    )
     while True:
         # Try and except blocks to catch timeout and break
         try:
             interaction = await bot.wait_for(
                 "button_click",
                 check=lambda i: i.component.id in ["back", "front"],  # You can add more
-                timeout=30.0  # 30 seconds of inactivity
+                timeout=30.0,  # 30 seconds of inactivity
             )
             # Getting the right list index
             if interaction.component.id == "back":
@@ -52,29 +67,61 @@ async  def pagination(dataset_with_embeds, ctx):
                 current = len(dataset_with_embeds) - 1
 
             # Edit to new page + the center counter changes
-            components = [[Button(label="Prev", id="back", style=ButtonStyle.red),
-                           Button(
-                               label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current]))}/{len(dataset_with_embeds)}",
-                               id="cur", style=ButtonStyle.green, disabled=True),
-                           Button(label="Next", id="front", style=ButtonStyle.green)]]
+            components = [
+                [
+                    discord_components.Button(
+                        label="Prev",
+                        id="back",
+                        style=discord_components.ButtonStyle.red,
+                    ),
+                    discord_components.Button(
+                        label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current]))}/{len(dataset_with_embeds)}",
+                        id="cur",
+                        style=discord_components.ButtonStyle.green,
+                        disabled=True,
+                    ),
+                    discord_components.Button(
+                        label="Next",
+                        id="front",
+                        style=discord_components.ButtonStyle.green,
+                    ),
+                ]
+            ]
 
             await interaction.edit_origin(
-                embed=dataset_with_embeds[current],
-                components=components
+                embed=dataset_with_embeds[current], components=components
             )
         except asyncio.TimeoutError:
             # Disable and get outta here
-            components = [[Button(label="Prev", id="back", style=ButtonStyle.green, disabled=True),
-                           Button(label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current])) + 1}/{len(dataset_with_embeds)}",
-                                  id="cur", style=ButtonStyle.grey, disabled=True),
-                           Button(label="Next", id="front", style=ButtonStyle.green, disabled=True)]]
-            await mainMessage.edit(
-                components=components
-            )
+            components = [
+                [
+                    discord_components.Button(
+                        label="Prev",
+                        id="back",
+                        style=discord_components.ButtonStyle.green,
+                        disabled=True,
+                    ),
+                    discord_components.Button(
+                        label=f"Page {int(dataset_with_embeds.index(dataset_with_embeds[current])) + 1}/{len(dataset_with_embeds)}",
+                        id="cur",
+                        style=discord_components.ButtonStyle.grey,
+                        disabled=True,
+                    ),
+                    discord_components.Button(
+                        label="Next",
+                        id="front",
+                        style=discord_components.ButtonStyle.green,
+                        disabled=True,
+                    ),
+                ]
+            ]
+            await mainMessage.edit(components=components)
             break
+
 
 async def on_ready():
     print("Bot Online")
+
 
 ## Loads the commands (Cogs) from each "context" ##
 bot.load_extension("general_commands")
